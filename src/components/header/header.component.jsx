bac161f322d90ed6444ba0a9c39b3ff80e87630c
HuyHQ <huyha14082012@gmail.com>
2020-10-09 15:15:58 +0700

    created cart-item and connected card dropdown with cartItems reducer

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -4,7 +4,7 @@ import { connect } from "react-redux";
 import { auth } from "../../firebase/firebase.utils";
 import { ReactComponent as Logo} from "../../assets/crown.svg";
 import CartIcon from "../cart-icon/cart-icon.component";
-import CartDropdown from "../cart-dropdown/card-dropdown.component";
+import CartDropdown from "../cart-dropdown/cart-dropdown.component";
 
 import './header.styles.scss';
 
