27a79b34294244315895316e3c546afc6b980ccd
HuyHQ <huyha14082012@gmail.com>
2020-10-11 17:56:46 +0700

    added addItem and removeItem funcitonality to checkout item component

--- src/redux/cart/cart.reducer.js
+++ src/redux/cart/cart.reducer.js
@@ -1,5 +1,7 @@
 import { CartActionTypes } from './cart.types';
-import { addItemToCart, clearItemFromCart } from "./cart.utils";
+import { addItemToCart,
+         clearItemFromCart,
+         removeItemFromCart } from "./cart.utils";
 
 
 const INITIAL_STATE  = {
@@ -18,6 +20,11 @@ const cartReducer = (state = INITIAL_STATE, action) => {
             return {
                 ...state,
                 cartItems: addItemToCart(state.cartItems, action.payload)
+            };
+        case CartActionTypes.REMOVE_ITEM:
+            return {
+                ...state,
+                cartItems: removeItemFromCart(state.cartItems, action.payload)
             };
         case CartActionTypes.CLEAR_ITEM_FROM_CART:
             return {
--- src/redux/cart/cart.utils.js
+++ src/redux/cart/cart.utils.js
@@ -1,11 +1,11 @@
 export const addItemToCart = (cartItems, cartItemToAdd) => {
-    const existingCartItem = cartItems.find(
-        cartItem => cartItem.id === cartItemToAdd.id
+    const existingCartItem = cartItems.find(cartItem =>
+        cartItem.id === cartItemToAdd.id
     );
 
     if (existingCartItem) {
-        return cartItems.map(
-            cartItem => cartItem.id === cartItemToAdd.id
+        return cartItems.map(cartItem =>
+            cartItem.id === cartItemToAdd.id
                 ? { ...cartItem, quantity: cartItem.quantity + 1 }
                 : cartItem
         )
@@ -16,5 +16,24 @@ export const addItemToCart = (cartItems, cartItemToAdd) => {
 
 export const clearItemFromCart = (cartItems, cartItemToRemove) => {
     return cartItems.filter(cartItem =>
-        cartItem.id !== cartItemToRemove.id)
+        cartItem.id !== cartItemToRemove.id
+    )
 };
+
+export const removeItemFromCart = (cartItems, cartItemToRemove) => {
+    const existingCartItem = cartItems.find(cartItem =>
+        cartItem.id === cartItemToRemove.id
+    );
+
+    if (existingCartItem.quantity === 1) {
+        return cartItems.filter(cartItem =>
+            cartItem.id !== cartItemToRemove.id
+        );
+    }
+
+    return cartItems.map(cartItem =>
+        cartItem.id === cartItemToRemove.id
+            ? {...cartItem, quantity: cartItem.quantity - 1}
+            : cartItem
+    );
+}
