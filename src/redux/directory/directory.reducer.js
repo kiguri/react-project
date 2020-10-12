2bc0640b9a2638d85f01c5f177add5400da98253
HuyHQ <huyha14082012@gmail.com>
2020-10-12 14:21:05 +0700

    created respective seleector and updated corresponding components with new redux flow for directory/shop

--- src/redux/directory/directory.reducer.js
+++ src/redux/directory/directory.reducer.js
@@ -5,35 +5,35 @@ const INITIAL_STATE = {
             imageUrl: 'https://i.ibb.co/cvpntL1/hats.png',
             size: '',
             id: 1,
-            linkUrl: 'hats'
+            linkUrl: 'shop/hats'
         },
         {
             title: 'jackets ',
             imageUrl: 'https://i.ibb.co/px2tCc3/jackets.png',
             size: '',
             id: 2,
-            linkUrl: ''
+            linkUrl: 'shop/jackets'
         },
         {
             title: 'sneakers',
             imageUrl: 'https://i.ibb.co/0jqHpnp/sneakers.png',
             size: '',
             id: 3,
-            linkUrl: ''
+            linkUrl: 'shop/sneakers'
         },
         {
             title: 'womens',
             imageUrl: 'https://i.ibb.co/GCCdy8t/womens.png',
             size: 'large',
             id: 4,
-            linkUrl: ''
+            linkUrl: 'shop/womens'
         },
         {
             title: 'mens',
             imageUrl: 'https://i.ibb.co/R70vBrQ/men.png',
             size: 'large',
             id: 5,
-            linkUrl: ''
+            linkUrl: 'shop/mens'
         },
     ]
 }
